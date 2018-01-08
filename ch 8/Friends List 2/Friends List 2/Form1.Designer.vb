870faf0f5f4b37ed1731b4646e276ca8ca4acb75
tylerconger5 <33033554+tylerconger5@users.noreply.github.com>
2018-01-08 11:14:12 -0600

    mm

---
+++ ch 8/Friends List 2/Friends List 2/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstGo = New System.Windows.Forms.ListBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnAll = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstGo
+        '
+        Me.lstGo.FormattingEnabled = True
+        Me.lstGo.Location = New System.Drawing.Point(152, 37)
+        Me.lstGo.Name = "lstGo"
+        Me.lstGo.Size = New System.Drawing.Size(120, 160)
+        Me.lstGo.TabIndex = 0
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(13, 103)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(27, 129)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnAll
+        '
+        Me.btnAll.Location = New System.Drawing.Point(180, 203)
+        Me.btnAll.Name = "btnAll"
+        Me.btnAll.Size = New System.Drawing.Size(75, 23)
+        Me.btnAll.TabIndex = 3
+        Me.btnAll.Text = "Show All"
+        Me.btnAll.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnAll)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lstGo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstGo As System.Windows.Forms.ListBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnAll As System.Windows.Forms.Button
+
+End Class
